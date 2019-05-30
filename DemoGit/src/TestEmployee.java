c10fa6f2fab9de4f856ff4a6622dce01b371c3d1
RickyTricks <raakiram7741@gmail.com>
2019-05-30 16:52:43 +0530

    Main method

---
+++ DemoGit/src/TestEmployee.java
@@ -0,0 +1,13 @@
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e1 = new Employee();
+		e1.setEmpId(102);
+		e1.setEmpName("Raaki");
+		e1.setEmpSal(20000);
+
+	}
+
+}
